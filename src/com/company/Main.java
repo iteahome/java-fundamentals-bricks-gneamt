793fae2ff0e15515c8328d6ec15703864fdf4019
george neamt <georgeneamt2004@yahoo.com>
2019-12-14 00:37:22 +0200

    add solution to bricks problem

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int numberOfBricks = 14;
+        int i = 1;
+        boolean theyStillHaveBricks = true;
+        while (numberOfBricks > 0 && theyStillHaveBricks) {
+            if (numberOfBricks <= i ){        //if they still have less than i bricks, Patlu will place them and will win
+                System.out.println("Patlu");  //print the winner, Patlu
+                theyStillHaveBricks = false;  //set the value to false to stop the iteration (while)
+            }
+            else if (i < numberOfBricks && numberOfBricks <= (i + 2*i)){  //if they still have more than i but less than 2*i, Motu will win
+                System.out.println("Motu");                               //Print the winner, Motu
+                theyStillHaveBricks = false;                              //set the value to false to stop the iteration (while)
+            }
+            else{
+                numberOfBricks = numberOfBricks - i; //Patlu puts i bricks
+                numberOfBricks = numberOfBricks - 2 * i; //Motu puts 2*i bricks
+                i++;
+            }
+        }
+    }
+}
